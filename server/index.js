8ecf9abb9ec5048984f673768ae2a47bbd8ec77a
Kyle Parry <kyle.parry.303@gmail.com>
2020-12-11 12:25:49 -0700

    Random quotes added to quotes array in server

--- server/index.js
+++ server/index.js
@@ -10,13 +10,12 @@ const headers = {
 
 const port = 3000;
 
-// TODO: Fill with strings of your favorite quotes :)
 const quotes = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
+  'I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best. -Marilyn Monroe',
+  'Be yourself; everyone else is already taken. -Oscar Wilde',
+  'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. -Albert Einstein',
+  'So many books, so little time. -Frank Zappa',
+  'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams. -Dr. Seuss'
 ];
 
 //Utility Function to return a random integer
